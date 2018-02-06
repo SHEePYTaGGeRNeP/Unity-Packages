94985b9f6180c7e4d13ce7a91e80655537dc3518
Lorenzo <sheepytaggernep@gmail.com>
2018-02-06 09:48:06 +0100

    Updated Assets for CustomEffects

---
+++ Unity Project For Exporting/Assets/Scripts/Effects/CustomEffect.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts.Effects
+{
+    using UnityEngine;
+
+    public abstract class CustomEffect  : MonoBehaviour
+    {
+        protected enum DestroyComponent { DestroyGameObject, DestroyComponent, No };
+        [SerializeField]
+        protected DestroyComponent destroyComponentAfterFinished;
+
+        [SerializeField]
+        protected bool playOnAwake;
+
+        public abstract float GetLength();
+        public abstract void Play();
+    }
+}
---
+++ Unity Project For Exporting/Assets/Scripts/Effects/CustomParticleEffect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Effects
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class CustomParticleEffect : CustomEffect
+    {
+        protected ParticleSystem _particleSystem;
+
+        protected virtual void Awake()
+        {
+            this._particleSystem = this.GetComponent<ParticleSystem>();
+            if (this.playOnAwake)
+                this.Play();
+        }
+
+        public override float GetLength()
+        {
+            return this._particleSystem.main.duration;
+        }
+
+        public override void Play()
+        {
+            this._particleSystem.Play();
+            switch (this.destroyComponentAfterFinished)
+            {
+                case DestroyComponent.DestroyComponent:
+                    Destroy(this, this._particleSystem.main.duration);
+                    break;
+                case DestroyComponent.DestroyGameObject:
+                    Destroy(this.gameObject, this._particleSystem.main.duration);
+                    break;
+                case DestroyComponent.No:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
